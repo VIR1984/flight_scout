80a8e37edc92b627333d4cc3a76bcd838dbac365
VIR1984 <mayer777@bk.ru>
2026-02-04 13:20:21 +0300

    fix: синтаксическая ошибка if not в start.py
    - Исправлены все случаи 'if not' -> 'if not data' в обработчиках кнопок
    - Устранена ошибка SyntaxError при импорте модуля

--- handlers/start.py
+++ handlers/start.py
@@ -338,7 +338,7 @@ async def handle_cheap_request(message: Message):
 async def show_top_offer(callback: CallbackQuery):
     cache_id = callback.data.split("_")[-1]
     data = await redis_client.get_search_cache(cache_id)
-    if not 
+    if not data: 
         await callback.answer("Данные устарели", show_alert=True)
         return
     
@@ -383,7 +383,7 @@ async def show_top_offer(callback: CallbackQuery):
 async def show_all_offers(callback: CallbackQuery):
     cache_id = callback.data.split("_")[-1]
     data = await redis_client.get_search_cache(cache_id)
-    if not 
+    if not data: 
         await callback.answer("Данные устарели", show_alert=True)
         return
     
@@ -450,7 +450,7 @@ async def show_all_offers(callback: CallbackQuery):
 async def show_direct_flights(callback: CallbackQuery):
     cache_id = callback.data.split("_")[-1]
     data = await redis_client.get_search_cache(cache_id)
-    if not 
+    if not data:
         await callback.answer("Данные устарели", show_alert=True)
         return
     
